5a93125db1fb1a46cf57350589dc37d7b8e117f7
Derrick Lin <drrckln@gmail.com>
2016-05-09 00:28:17 -0700

    vectorized operations

--- TheArtofR.R
+++ TheArtofR.R
@@ -19,4 +19,32 @@ first1 <- function(x) {
 
 # Arrays and matrices are vectors, with extra class attributes
 
+preda = function(x,k) {
+  n = length(x)
+  k2 = k/2
+  pred = vector(length=n-k) # will contain predicted values
+  for (i in 1:(n-k)) {
+    if (sum(x[i:(i+(k-1))]) >= k2) {
+      pred[i] = 1
+    else pred[i] = 0
+    }
+  }
+  return(mean(abs(pred-x[(k+1):n])))
+}
 
+predb = function(x,k) {
+  n = length(x)
+  k2 = k/2
+  pred = vector(length=n-k)
+  sm = sum(x[1:k])
+  if (sm >= k2) pred[i] = 1 else pred[i] = 0
+  if (n-k >= 2) {
+    for (i in 2:(n-k)) {
+      sm = sm + x[i+k-1] - x[i-1]
+      if (sm >= k2) pred[i] = 1 else pred[i] = 0
+    }
+  }
+  return(mean(abs(pred-x[(k+1):n])))
+}
+
+# or.. use cumsum(), which is cumulative sum
